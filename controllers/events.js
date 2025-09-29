8375c446d2b8896e021f94a137081c16e4b26fc5
hawra <hawra.yusuf02@outlook.com>
2025-09-29 18:48:48 +0300

    fixed issue with checklist not showing

--- controllers/events.js
+++ controllers/events.js
@@ -33,12 +33,18 @@ router.get('/new', async (req, res) => {
 router.post('/', async (req, res) =>{
   try{
     const currentUser = await User.findById(req.session.user._id);
-    const checklistArray = Array.isArray(req.body.checklist) ? req.body.checklist.map(item => ({
-      task: item.task,
-      isDone: item.isDone === 'true'
+    const taskList = req.body.taskList
+    const isDoneList = req.body.isDoneList
+    
+    console.log(req.body.taskList);
+    console.log(req.body.isDoneList);
+    const checklistArray = Array.isArray(taskList) ? taskList.map((task, idx) => ({
+      task,
+      isDone: isDoneList[idx] === 'yes'
     }))
     : [];
 
+    console.log(checklistArray)
     const newEvent = {
       title: req.body.title,
       date: req.body.date,
